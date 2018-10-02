b2b6c59ae90e6e242519b7550966e8c46a066d4f
David Jungst <djungst@gmail.com>
2018-10-02 11:10:01 -0600

    add dropzone

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,18 +6,86 @@ import {
   Container,
   Image,
   Header,
+  Form,
 } from 'semantic-ui-react'
 import { connect } from 'react-redux'
+import Dropzone from 'react-dropzone'
 
 const defaultImage = 'https://d30y9cdsu7xlg0.cloudfront.net/png/15724-200.png'
 
 class Profile extends React.Component {
-  state = { editing: false }
-
-  editView = () => {
+  state = { 
+    editing: false, 
+    formValues: { name: '', email: '', file: '' },
   }
 
-  profileView = () => {
+  static getDerivedStateFromProps(props, state) {
+    const { user } = props
+    const { formValues, editing } = state
+    if (user.name !== formValues.name && !editing) {
+      return { formValues: { name: user.name, email: user.email } }
+    }
+  }
+
+  onDrop = (files) => {
+    this.setState( state => {
+      return {
+        formValues: {
+          ...state.formValues,
+          file: files[0]
+        }
+      }
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState( state => { 
+      return {
+        formValues: {
+          ...state.formValues,
+          [name]: value
+        }
+      }
+    })
+  }
+
+  editView = () => {
+    const { formValues: { name, email, file } } = this.state
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Grid.Column width={4}>
+          <Dropzone
+            onDrop={this.onDrop}
+            multiple={false}
+          >
+            { file && <Image src={file.preview} alt="upload preview" /> }
+          </Dropzone>
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <Form.Input
+            label="Name"
+            name="name"
+            value={name}
+            required
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label="Email"
+            name="email"
+            value={email}
+            required
+            onChange={this.handleChange}
+            type="email"
+          />
+          <Button>Update</Button>
+        </Grid.Column>
+      </Form>
+    )
   }
 
   toggleEdit = () => {
